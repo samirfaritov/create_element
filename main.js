95f2df70ac25b4bbacbebe4a2789849d7d8d98fb
samirfaritov <samirfaritov6@gmail.com>
2023-11-27 16:55:22 +0500

    Xatto to'g'ri shud

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
 const content = document.querySelector('.content')
 const addBtn = document.querySelector('#add')
 
-let counter = 1
+let counter = ' '
 
-let colors = [
-    'red',
-    'green',
-    'yellow',
-    'blue',
-    'black',
-    'tomato',
-    'chocolate',
-    'darkblue',
-    'darkgreen',
-    'darkyellow'
+let img = [
+    'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?cs=srgb&dl=pexels-pixabay-36717.jpg&fm=jpg',
+    'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg',
+    'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg',
+    'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg',
+    'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700438400&semt=ais',
+    'https://media.istockphoto.com/id/1198271727/photo/abstract-wavy-object.jpg?s=612x612&w=0&k=20&c=CuuC79GvcO06K5EpBeQ2AMO68TkZS8mw9ZydJ2BMfeo=',
+    'https://images8.alphacoders.com/133/1331088.png',
+    'https://img.freepik.com/premium-photo/closeup-nature-view-colorful-leaves_860528-2622.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1697241600&semt=ais',
+    'https://image.winudf.com/v2/image1/Y29tLm5hdHVyYWxpbWFnZWhkd2FsbHBhcGVyLnJ5cnNfc2NyZWVuXzBfMTU0MjM3MDgxNl8wODU/screen-0.webp?fakeurl=1&type=.webp',
+    'https://image.winudf.com/v2/image1/Y29tLm5hdHVyYWxpbWFnZWhkd2FsbHBhcGVyLnJ5cnNfc2NyZWVuXzBfMTU0MjM3MDgxNl8wODU/screen-0.webp?fakeurl=1&type=.webp'
 ]
-
-let chars = '0123456789abcdef'
-
-let a = chars.split('')
-console.log(a);
+const conteiner = document.querySelector('.conteiner')
+console.log(conteiner);
 
 
 
 addBtn.addEventListener('click', () => {
-    let rnd = Math.floor(Math.random() * a.length)
-    let rnd2 = Math.floor(Math.random() * a.length)
-    let rnd3 = Math.floor(Math.random() * a.length)
-    let rnd4 = Math.floor(Math.random() * a.length)
-    let rnd5 = Math.floor(Math.random() * a.length)
-    let rnd6 = Math.floor(Math.random() * a.length)
+    let rnd = Math.floor(Math.random() * img.length)
+    console.log(rnd);
 
     let box = document.createElement('div')
     box.classList.add('box')
-    // box.style.backgroundColor = colors[rnd]
-    box.style.backgroundColor = `#${a[rnd]}${a[rnd2]}${a[rnd3]}${a[rnd4]}${a[rnd5]}${a[rnd6]}`
+    box.style.backgroundImage = `url(${img[rnd]})`
     box.innerHTML = counter
-    content.appendChild(box)
+    conteiner.appendChild(box)
 
     counter++
+
     console.log(box);
 })
+
+    // const imgg = document.querySelector(".img")
+    // // imgg.classList.add('img')
+    // imgg.style.backgroundImage = "url('https://image.winudf.com/v2/image1/Y29tLm5hdHVyYWxpbWFnZWhkd2FsbHBhcGVyLnJ5cnNfc2NyZWVuXzBfMTU0MjM3MDgxNl8wODU/screen-0.webp?fakeurl=1&type=.webp')"
